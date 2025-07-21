7d6f8760763b97c850a2cae0b002682130458bfc
Samiul Sk <sam91v@gmail.com>
2025-07-21 13:29:44 +0530

    feat: add basic chi middlewares (RequestID, Logger, Recoverer)

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,8 +2,10 @@ package router
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/samims/hcaas/internal/handler"
 )
@@ -11,11 +13,21 @@ import (
 func NewRouter(h *handler.URLHandler) http.Handler {
 	r := chi.NewRouter()
 
+	// Middleware
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(30 * time.Second))
+
 	r.Route("/urls", func(r chi.Router) {
 		r.Get("/", h.GetAll)
 		r.Get("/{id}", h.GetByID)
 		r.Post("/", h.Add)
 		r.Put("/{id}", h.UpdateStatus)
+	})
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
 	})
 	return r
 }
