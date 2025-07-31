4acd57aeffd77dcebaa64332b1905c37053b5c5b
Samiul Sk <sam91v@gmail.com>
2025-07-31 16:24:24 +0530

    feat: primary strcture

--- services/notification/go.mod
+++ services/notification/go.mod
@@ -1,3 +1,5 @@
 module github.com/samims/hcaas/services/notification
 
 go 1.24.4
+
+require github.com/go-chi/chi/v5 v5.2.2
--- services/notification/internal/service/notification_service.go
+++ services/notification/internal/service/notification_service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/samims/hcaas/services/notification/internal/model"
+)
+
+type NotificationService interface {
+	SendNotification(ctx context.Context, notification *model.Notification) error
+}
+
+type notificationService struct {
+}
+
+func NewNotificationService() NotificationService {
+	return &notificationService{}
+}
+
+// SendNotification sends the notification
+func (s *notificationService) SendNotification(ctx context.Context, notification *model.Notification) error {
+	// Simulate sending notification service
+	fmt.Println("Notification: URL", notification.UrlId)
+	return nil
+}
