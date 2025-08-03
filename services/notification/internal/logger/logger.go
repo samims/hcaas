e12d63512fe72493e9fa4fb49a04ea61a5da1ea3
Samiul Sk <sam91v@gmail.com>
2025-08-03 17:41:05 +0530

    feat: add logger implementation

--- services/notification/internal/logger/logger.go
+++ services/notification/internal/logger/logger.go
@@ -1,1 +1,16 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+// NewLogger initializes and returns a structured logger using slog.
+// It outputs JSON-formatted logs to stdout, suitable for production.
+func NewLogger() *slog.Logger {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: false,
+	})
+	logger := slog.New(handler)
+	return logger
+}
